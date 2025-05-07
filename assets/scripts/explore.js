ffe82be22d818dbd8f00165276d992ebc4d4e316
mazen-hamdi <mhamdi@ucsd.edu>
2025-05-06 21:05:15 -0700

    Add feature populate voices()

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,27 @@
-// explore.js
-
-window.addEventListener('DOMContentLoaded', init);
-
-function init() {
-  // TODO
+function populateVoices() {
+  const voiceSelect = document.getElementById('voice-select');
+  voiceSelect.innerHTML = '';    // clear old
+  speechSynthesis.getVoices().forEach(v => {
+    const opt = document.createElement('option');
+    opt.value = v.name;
+    opt.textContent = `${v.name} (${v.lang})`;
+    voiceSelect.appendChild(opt);
+  });
 }
+window.speechSynthesis.onvoiceschanged = populateVoices;
+
+
+const speakBtn = document.getElementById('press-to-talk');
+const textArea = document.getElementById('text-to-speak');
+const faceImg  = document.getElementById('face');
+
+speakBtn.addEventListener('click', () => {
+  const utter = new SpeechSynthesisUtterance(textArea.value);
+  const voiceName = document.getElementById('voice-select').value;
+  utter.voice = speechSynthesis.getVoices().find(v => v.name === voiceName);
+
+  utter.onstart = () => faceImg.src = 'assets/images/smiling-open.png';
+  utter.onend   = () => faceImg.src = 'assets/images/smiling.png';
+
+  speechSynthesis.speak(utter);
+});
